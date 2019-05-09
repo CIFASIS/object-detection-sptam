a1d27c5b1b72d4d729189627648f1ca2174da5c2
Erica <vidal@cifasis-conicet.gov.ar>
2019-05-09 09:55:21 -0300

    mod Javier

---
+++ examples/CMakeFiles/convert_mnist_data.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/taihu/Documentos/jcorti_bkp/rcnn/py-faster-rcnn/caffe-fast-rcnn/examples/mnist/convert_mnist_data.cpp" "/home/taihu/Documentos/jcorti_bkp/rcnn/py-faster-rcnn/caffe-fast-rcnn/examples/CMakeFiles/convert_mnist_data.dir/mnist/convert_mnist_data.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CAFFE_VERSION=1.0.0-rc4"
+  "GTEST_USE_OWN_TR1_TUPLE"
+  "USE_CUDNN"
+  "USE_LEVELDB"
+  "USE_LMDB"
+  "USE_OPENCV"
+  "WITH_PYTHON_LAYER"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "/usr/local/include"
+  "include"
+  "/usr/include/hdf5/serial"
+  "/usr/local/cuda/include"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/include/atlas"
+  "/usr/include/python2.7"
+  "/home/taihu/.local/lib/python2.7/site-packages/numpy/core/include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/taihu/Documentos/jcorti_bkp/rcnn/py-faster-rcnn/caffe-fast-rcnn/src/caffe/CMakeFiles/caffe.dir/DependInfo.cmake"
+  "/home/taihu/Documentos/jcorti_bkp/rcnn/py-faster-rcnn/caffe-fast-rcnn/src/caffe/CMakeFiles/proto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
