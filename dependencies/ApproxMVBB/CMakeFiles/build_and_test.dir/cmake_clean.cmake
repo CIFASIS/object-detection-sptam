023db200ac0b26cc21652231e4fe71c4ba16151b
evidal <vidal@cifasis-conicet.gov.ar>
2019-05-20 15:12:41 -0300

    clean

--- dependencies/ApproxMVBB/CMakeFiles/build_and_test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build_and_test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build_and_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- dependencies/ApproxMVBB/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB
-# Build directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib)
-subdirs(tests)
-subdirs(example)
