f2ba40ca9aec9d9164fa7a718938e479a865b290
evidal <vidal@cifasis-conicet.gov.ar>
2019-05-20 14:55:44 -0300

    clean ApproxMVBB

--- dependencies/ApproxMVBB/thirdparty/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB/thirdparty/googletest-src/googletest
-# Build directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB/thirdparty/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
