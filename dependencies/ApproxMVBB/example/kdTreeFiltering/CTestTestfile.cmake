756bc600cdf800c31c35eb187459ccaa2dcffe9a
evidal <vidal@cifasis-conicet.gov.ar>
2019-05-22 16:11:13 -0300

    clean

--- dependencies/ApproxMVBB/example/kdTreeFiltering/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB/example/kdTreeFiltering
-# Build directory: /home/taihu/Documentos/jcorti_bkp/ApproxMVBB2/ApproxMVBB/example/kdTreeFiltering
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/ApproxMVBB/parallelmake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cores=$(grep -c ^processor /proc/cpuinfo)
-echo "Building with $cores cores!"
-make -j$cores "$@" 
-exit
