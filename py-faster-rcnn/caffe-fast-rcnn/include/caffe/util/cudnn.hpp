cc459893f474dce035d42ab07c40cb66b8c13b9f
evidal <vidal@cifasis-conicet.gov.ar>
2019-05-22 11:50:41 -0300

    fix dependencies and cdnn version

--- py-faster-rcnn/caffe-fast-rcnn/include/caffe/util/cudnn.hpp
+++ py-faster-rcnn/caffe-fast-rcnn/include/caffe/util/cudnn.hpp
@@ -118,7 +118,7 @@ template <typename Dtype>
 inline void setConvolutionDesc(cudnnConvolutionDescriptor_t* conv,
     cudnnTensorDescriptor_t bottom, cudnnFilterDescriptor_t filter,
     int pad_h, int pad_w, int stride_h, int stride_w) {
-#if CUDNN_VERSION_MIN(7, 1, 5)
+#if CUDNN_VERSION_MIN(7, 1, 4)
   CUDNN_CHECK(cudnnSetConvolution2dDescriptor(*conv,
       pad_h, pad_w, stride_h, stride_w, 1, 1, CUDNN_CROSS_CORRELATION, dataType<Dtype>::type));
 #else
