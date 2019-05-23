846b27de1eadcc32c05a9b5ceee1a4e0d1cf3d50
evidal <vidal@cifasis-conicet.gov.ar>
2019-05-23 11:34:43 -0300

    getG2o

--- dependencies/getG2o.sh
+++ dependencies/getG2o.sh
@@ -3,4 +3,4 @@
 rm -Rf g2o
 git clone https://github.com/RainerKuemmerle/g2o.git
 cd g2o
-git checkout 4b9c2f5b68d14ad479457b18c5a2a0bce1541a90
+git checkout 2e3566968c1f0e5608155a2a26154b98d6d752dc
