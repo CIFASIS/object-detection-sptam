02db02071ae7772bf0f91a7897bd3f712702e5ea
evidal <vidal@cifasis-conicet.gov.ar>
2020-08-21 19:42:11 -0300

    Correct source path in makefile

--- py-faster-rcnn/lib/setup.py
+++ py-faster-rcnn/lib/setup.py
@@ -132,7 +132,7 @@ ext_modules = [
         # we're only going to use certain compiler args with nvcc and not with
         # gcc the implementation of this trick is in customize_compiler() below
         extra_compile_args={'gcc': ["-Wno-unused-function"],
-                            'nvcc': ['-arch=sm_35',
+                            'nvcc': ['-arch=sm_52',
                                      '--ptxas-options=-v',
                                      '-c',
                                      '--compiler-options',
